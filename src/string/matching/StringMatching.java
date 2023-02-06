f71569c62babe8da9b332a0d8ffb2e91ce326d1a
KhaledMAlasmari <khaledmdalasmari@gmail.com>
2023-02-06 17:17:08 +0300

    Shift table adds distinct characters and fixed the output

--- src/string/matching/StringMatching.java
+++ src/string/matching/StringMatching.java
@@ -1,13 +1,16 @@
+/*
+
+    Khaled mohammed alasmari - 2035189
+    Omar abdulbagi - 2037070
+    Salman alhothly - 2044556
+
+*/
+
 package string.matching;
-
-import java.util.ArrayList;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Map;
 import java.util.HashMap;
 import java.util.Random;
 import java.io.FileWriter;
@@ -15,10 +18,10 @@ import java.io.IOException;
 
 public class StringMatching {
 
-    private static HashMap<String, Integer> shiftTable;
+    private static HashMap<Character, Integer> shiftTable;
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Scanner inputFile = new Scanner(new File("C:\\Users\\ramob\\OneDrive\\Desktop\\input.txt"));
+        Scanner inputFile = new Scanner(new File("input.txt"));
         Scanner input = new Scanner(System.in);
         FileWriter patternsFile = new FileWriter("patterns.txt");
         shiftTable = new HashMap<>();
@@ -56,8 +59,7 @@ public class StringMatching {
                 reading.append(inputFile.nextLine());
             }
         }
-        String text = reading.toString();
-        return text.toLowerCase();
+        return reading.toString().toLowerCase();
     }
 
     public static String[] genPatterns(int numOfLines, int numOfPatterns, int LengthOfPatterns, String text) {
@@ -74,61 +76,41 @@ public class StringMatching {
         return patterns;
     }
 
-    public static HashMap<String, Integer> createShiftTable(String pattern, int lengthOfPattern) {
+    public static void createShiftTable(String pattern, int lengthOfPattern, String text) {
         //initialize shift table chareters with length of pattern
-        shiftTable.clear();
-        for (char ch = 'a'; ch <= 'z'; ++ch) {
-            shiftTable.put(String.valueOf(ch), lengthOfPattern);
-
+        int textLength = text.length();
+        for (int i = 0; i < textLength; i++) {
+            char ch = text.charAt(i);
+            if (!shiftTable.containsKey(ch)) {
+                shiftTable.put(ch, lengthOfPattern);
+            }
         }
-        for (int i = 0; i < 10; i++) {
-            shiftTable.put(String.valueOf(i), lengthOfPattern);
-
-        }
-        shiftTable.put(",", lengthOfPattern);
-        shiftTable.put(".", lengthOfPattern);
-        shiftTable.put(" ", lengthOfPattern);
-        shiftTable.put("\"", lengthOfPattern);
-        shiftTable.put("?", lengthOfPattern);
-        shiftTable.put("!", lengthOfPattern);
-        shiftTable.put("(", lengthOfPattern);
-        shiftTable.put(")", lengthOfPattern);
-        shiftTable.put("-", lengthOfPattern);
-        shiftTable.put("'", lengthOfPattern);
 
         //implement algorithm
         for (int i = 0; i < lengthOfPattern - 1; i++) {
-            shiftTable.put(String.valueOf(pattern.substring(i, i + 1)), lengthOfPattern - 1 - i);
+            shiftTable.put(pattern.charAt(i), lengthOfPattern - 1 - i);
         }
         System.out.println(shiftTable);
-
-        return shiftTable;
     }
 
-    public static int horspool(String pattern, String text, int lengthOfPattern) {
-
-        System.out.println(pattern);
-        shiftTable = createShiftTable(pattern, lengthOfPattern);
-        int i = lengthOfPattern;
-        while (i <= text.length()) {
+    public static int horspool(String pattern, String text, int m) {
+        int i = m - 1;
+        for (; i < text.length();) {
             int k = 0;
-            while ((k < lengthOfPattern) && (pattern.substring((lengthOfPattern - 1) - k, (lengthOfPattern - 1) - k + 1).equals(text.substring(i - k - 1, i - k)))) {
+            while ((k < m) && (pattern.charAt(m - 1 - k) == (text.charAt(i - k)))) {
                 k++;
             }
-
-            if (k == lengthOfPattern) {
-                return i - lengthOfPattern;
+            if (k == m) {
+                return i - m + 1;
             } else {
-                i = i + shiftTable.get(String.valueOf(text.charAt(i)));
+                i = i + shiftTable.get(text.charAt(i));
             }
         }
-
         return -1;
     }
 
     public static int bruteForce(String pattern, String text, int lengthOfPattern) {
         for (int i = 0; i <= text.length() - lengthOfPattern; i++) {
-
             int j = 0;
             while ((j < lengthOfPattern) && (pattern.substring(j, j + 1).equals(text.substring(i + j, j + i + 1)))) {
                 j++;
@@ -146,31 +128,38 @@ public class StringMatching {
         long startTime, endTime, timeTaken;
 
         for (String pattern : patterns) {
+            shiftTable.clear();
+            System.out.printf("The shift table for \"%s\" is:\n", pattern);
             startTime = System.nanoTime();
+            createShiftTable(pattern, pattern.length(), text);
             horspool(pattern, text, lengthOfPattern);
             endTime = System.nanoTime();
             timeTaken = endTime - startTime;
             horspoolTime.add(timeTaken);
-
+            
             startTime = System.nanoTime();
             bruteForce(pattern, text, lengthOfPattern);
             endTime = System.nanoTime();
             timeTaken = endTime - startTime;
             bruteForceTime.add(timeTaken);
+            System.out.println("");
         }
-        System.out.printf("average time of search in brute force approach: %d.2 \n", getaverage(bruteForceTime));
-        System.out.printf("average time of search in horspool approach: %d.2 \n", getaverage(horspoolTime));
-        
-        if(getaverage(bruteForceTime) < getaverage(horspoolTime))
-            System.out.println("for this instance brute force aproach is better than horspool ");
-        else System.out.println("for this instance brute force aproach is better than horspool ");
+        System.out.printf("average time of search in brute force approach: %,d ns \n", getaverage(bruteForceTime));
+        System.out.printf("average time of search in horspool approach: %,d ns \n", getaverage(horspoolTime));
+
+        if (getaverage(bruteForceTime) < getaverage(horspoolTime)) {
+            System.out.println("for this instance Brute Force approach is better than Horspool");
+        } else {
+            System.out.println("for this instance Horspool approach is better than Brute Force");
+        }
     }
-    public static long getaverage(ArrayList<Long> time){
-        
-        long sum = 0 ;
-        for(int i =0;i<time.size();i++){
-        sum = time.get(i) +sum;
-    }
+
+    public static long getaverage(ArrayList<Long> time) {
+
+        long sum = 0;
+        for (int i = 0; i < time.size(); i++) {
+            sum = time.get(i) + sum;
+        }
         return sum / time.size();
     }
 }
